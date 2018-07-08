fc3e7d595d204663b83a134a2b184f5d63f87fc4
JQ <kamahl437@gmail.com>
2018-07-08 10:55:21 -0400

    lodash and my scene
    also started trying to add typescript but I am failing so grabbing another repo with ts added

--- src/main.js
+++ src/main.js
@@ -2,27 +2,34 @@ import 'phaser';
 import BootScene from './scenes/BootScene';
 import GameScene from './scenes/GameScene';
 import TitleScene from './scenes/TitleScene';
+import _ from 'lodash';
 
-const config = {
-    // For more settings see <https://github.com/photonstorm/phaser/blob/master/src/boot/Config.js>
-    type: Phaser.WEBGL,
-    pixelArt: true,
-    roundPixels: true,
-    parent: 'content',
-    width: 400,
-    height: 240,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 800 },
-            debug: false
-        }
-    },
-    scene: [
-        BootScene,
-        TitleScene,
-        GameScene
-    ]
-};
+//thee original config for the mario game:
+// const config = {
+//     // For more settings see <https://github.com/photonstorm/phaser/blob/master/src/boot/Config.js>
+//     type: Phaser.WEBGL,
+//     pixelArt: true,
+//     roundPixels: true,
+//     parent: 'content',
+//     width: 400,
+//     height: 240,
+//     physics: {
+//         default: 'arcade',
+//         arcade: {
+//             gravity: { y: 800 },
+//             debug: false
+//         }
+//     },
+//     scene: [
+//         BootScene,
+//         TitleScene,
+//         GameScene
+//     ]
+// };
+// console.log('I am in the file man');
+// let exArr = [1,2,3,4,5,6,7,8,9];
+// _.each(exArr, (e) => {
+//     console.log(e);
+// })
 
 const game = new Phaser.Game(config);
---
+++ src/scenes/LinkedScene.js
@@ -0,0 +1,3 @@
+var config = {
+    type: Phaser.AUTO
+}
